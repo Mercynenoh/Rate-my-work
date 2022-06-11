efa48cc405609461bfae60f7d061d620c0fdef91
Mercynenoh <mercychepngenoh74@gmail.com>
2022-06-11 22:51:50 +0300

    Add base.html and navbar.html

--- projects/urls.py
+++ projects/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
