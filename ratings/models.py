11f2255dd7b0fdf0d4c464b90a78030a742d08d8
Mercynenoh <mercychepngenoh74@gmail.com>
2022-06-11 22:25:26 +0300

    Create project model

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    image = models.ImageField(upload_to = 'articles/',default='IMAGE')
+    title =  models.CharField(max_length =30)
+    description = models.TextField()
+    link = models.CharField(max_length =30)
