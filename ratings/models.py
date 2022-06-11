f2cb81daff72847a47fb7fc08ec72e899cb309f0
Mercynenoh <mercychepngenoh74@gmail.com>
2022-06-11 22:30:25 +0300

    Create profile model

--- ratings/models.py
+++ ratings/models.py
@@ -6,3 +6,10 @@ class Project(models.Model):
     title =  models.CharField(max_length =30)
     description = models.TextField()
     link = models.CharField(max_length =30)
+
+class Profile(models.Model):
+    image = models.ImageField(upload_to = 'articles/',default='IMAGE')
+    bio =  models.CharField(max_length =100)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    email = models.CharField(max_length =30)
+    phone = models.IntegerField(max_length =10)
