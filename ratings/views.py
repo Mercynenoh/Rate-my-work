43098284b128782f88206ce15b1202660f35a476
Mercynenoh <mercychepngenoh74@gmail.com>
2022-06-11 23:40:47 +0300

    Create project serializer

--- ratings/views.py
+++ ratings/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def apiOverView(request):
+    api_urls ={
+        'List':"/project-list/",
+        'Detail View':"/project-detail/<int:id>",
+        'Create':"/project-create",
+        'Update':"/project-update/<int:id>",
+        'Delete':"/project-delete/<int:id>",
+
+    }
+
+    return Response('api_urls');
+      
+
+    
